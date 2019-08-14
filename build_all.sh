b5200910c06c83187c229fa88b4bff0468e54a94
cthirase <cthirase@gmail.com>
2019-08-15 01:12:46 +0900

    Review the flow of synchronizing source files. Add cmake command.

--- build_all.sh
+++ build_all.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-git clone https://github.com/FFmpeg/FFmpeg.git src
+function on_error() {
+    exit 1
+}
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+FFSRC_DIR=$SCRIPT_DIR/src
+
+###########################################################
+# Get FFmpeg sources from GitHub
+if [ ! -d src ]; then
+    # [1st time] Get sources
+    git clone https://github.com/FFmpeg/FFmpeg.git $FFSRC_DIR || on_error
+else
+    # [Next time] Sync sources
+    git -C $FFSRC_DIR pull || on_error
+fi
+
+###########################################################
+# CMake
+cmake -S . -B make/gnumake || on_error
+cmake --build make/gnumake || on_error
